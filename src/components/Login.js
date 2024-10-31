1a7ac27a3cdffaa52e6263c8fe32ef438397173c
Chamir <samir.castillo.r@gmail.com>
2024-10-30 23:41:51 -0500

    Creacion de Register y Login conectado y verificado con la api en local

---
+++ src/components/Login.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import API_BASE_URL from '../config';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  
+    const formData = new URLSearchParams();
+    formData.append("Email", email);
+    formData.append("Password", password);
+  
+    try {
+      const response = await fetch(`${API_BASE_URL}/api/auth/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: formData.toString()
+      });
+  
+      if (response.ok) {
+        const data = await response.json();
+        alert("Login exitoso");
+        console.log("Token:", data.token);
+
+      } else {
+        const errorData = await response.json();
+        setErrorMessage(errorData.message || "Error al iniciar sesión");
+      }
+    } catch (error) {
+      setErrorMessage("Error de conexión con el servidor");
+      console.error("Error en handleLogin:", error);
+    }
+  };
+  
+  return (
+    <div className="login-container">
+      <h1>SamAn </h1>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">LOGIN</button>
+        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+      </form>
+    </div>
+  );
+}
+
+export default Login;
